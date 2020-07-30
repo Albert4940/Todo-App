40fd45e6d07310bce3cf7004078e1eac986a7567
Albert4940 <dorcealbertmary@gmail.com>
2020-07-30 11:55:22 -0700

    I added getCookie function

--- app/js/coockieManager.js
+++ app/js/coockieManager.js
@@ -4,3 +4,19 @@ function setCoockie(cname,cvalue,exdays){
 	var expires = "expires=" + d.toGMTString();
 	document.coockie = cname + "=" + value + ";" + expires + ";path=/";
 }
+
+function getCookie(cname){
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i < ca.length; i++){
+		var c = ca[i];
+		while(c.charAt(0) == ' '){
+			c = c.substring(1);
+		}
+		if(c.indexOf(name) == 0){
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
